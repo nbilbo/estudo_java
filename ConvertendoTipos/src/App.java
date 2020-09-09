5b758b7e63f8d4876103e11d02d62d1904550fe5
nbilbo <ddtank68@gmail.com>
2020-09-09 00:33:14 -0300

    ConvertendoTipos criado

---
+++ ConvertendoTipos/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args)  {
+        String valor;
+        Float idade;
+        Integer nota;
+
+        valor = "20.5";
+        idade = Float.parseFloat(valor);
+        
+        valor = "20";
+        nota = Integer.parseInt(valor);
+
+    }
+}
